10ea21bd47c8b4cbd53edbd552f83133a5a55793
Enna Phan <ennap@uci.edu>
2023-02-16 15:08:48 -0800

    add word_tokenize and Porter stemmer

--- indexer.py
+++ indexer.py
@@ -1,13 +1,25 @@
 import json
 import os
 from bs4 import BeautifulSoup
+from nltk.tokenize import word_tokenize
+from nltk.stem import *
 
 
 # Read through JSON file, create docID, parse content with listed encoding, tokenize,
 # stemming and other language processing, add doc as postings to inverted index (dictionary)
+index = dict()
+docID = 0
+
+stemmer = PorterStemmer()
 
 for file in os.listdir("cyberclub_ics_uci_edu"):
     with open("cyberclub_ics_uci_edu/" + file) as f:
         data = json.load(f)
-        soup = BeautifulSoup(data['content'], 'lxml', from_encoding = data['encoding'])
-        print(soup.get_text())
+        soup = BeautifulSoup(data['content'].encode(data['encoding']), 'lxml', from_encoding = data['encoding'])
+    tokens = word_tokenize(soup.get_text())
+    
+    stemmed = [stemmer.stem(token) for token in tokens]
+    print(stemmed)
+            
+        
+        
