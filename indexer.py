4204f1c3eddb33466de9e6958b2ac15b557f34a6
Enna Phan <ennap@uci.edu>
2023-02-16 18:27:41 -0800

    File and main index

--- indexer.py
+++ indexer.py
@@ -3,27 +3,38 @@ import os
 from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize
 from nltk.stem.snowball import SnowballStemmer
+from collections import defaultdict
 import re
 
 
 # Read through JSON file, create docID, parse content with listed encoding, tokenize,
 # stemming and other language processing, add doc as postings to inverted index (dictionary)
-index = dict()
+main_index = defaultdict(list)
 docID = 0
 
 stemmer = SnowballStemmer("english", ignore_stopwords=True)
 
 for file in os.listdir("cyberclub_ics_uci_edu"):
+    file_index = defaultdict(int)
+    
     with open("cyberclub_ics_uci_edu/" + file) as f:
         data = json.load(f)
         soup = BeautifulSoup(data['content'].encode(data['encoding']), 'lxml', from_encoding = data['encoding'])
-    tokens = word_tokenize(soup.get_text())
+        tokens = word_tokenize(soup.get_text())
     
     for token in tokens:
         alphanum = re.sub(r'[^a-zA-Z0-9]', '', token)
+        
         if len(alphanum) != 0:
             stem = stemmer.stem(alphanum)
-        print(f'Token: {token}, Stem: {stem}')
+        #print(f'Token: {token}, Stem: {stem}')
+        
+        file_index[token] += 1
+    
+    for token, freq in file_index.items():
+        main_index[token].append((docID, freq))
+
+print(main_index)
         
 
             
